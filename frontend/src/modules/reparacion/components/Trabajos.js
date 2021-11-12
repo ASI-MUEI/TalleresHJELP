cde7e4990b534a370ddb724fb1a58aeb480f81c8
Jose Figueiras <50220451+josefigueirasm@users.noreply.github.com>
2021-11-12 16:02:57 +0100

    Título de lista de trabajos

--- frontend/src/modules/reparacion/components/Trabajos.js
+++ frontend/src/modules/reparacion/components/Trabajos.js
@@ -2,52 +2,61 @@ import {FormattedDate, FormattedMessage, useIntl} from "react-intl";
 import {Link} from "react-router-dom";
 import {Badge} from "react-bootstrap";
 import commonFunctions from "../../commons/functions";
+import Container from "react-bootstrap/Container";
 
 const Trabajos = ({listaTrabajos}) =>{
 
     const intl = useIntl();
 
     return(
-        <table className="table table-striped table-hover">
-            <thead>
-            <tr>
-                <th scope="col">
-                    <FormattedMessage id='trabajo.matricula' />
-                </th>
-                <th scope="col">
-                    <FormattedMessage id='trabajo.cliente' />
-                </th>
-                <th scope="col">
-                    <FormattedMessage id='trabajo.estado' />
-                </th>
-                <th scope="col">
-                    <FormattedMessage id='trabajo.fechaCreacion' />
-                </th>
-            </tr>
-            </thead>
-
-            <tbody>
-            {listaTrabajos.map(trabajo =>
-                <tr key={trabajo.matricula}>
-                    <td>
-                        <Link to={`trabajos/${trabajo.idTrabajo}`}>{trabajo.matricula}</Link>
-                    </td>
-                    <td>
-                        {trabajo.nombreCliente}
-                    </td>
-                    <td className="align-middle">
-                        <Badge variant={commonFunctions.tipoLabelTrabajo(trabajo.estado)}>
-                            {intl.formatMessage({ id: 'trabajo.estado.' + trabajo.estado })}
-                        </Badge>
-                    </td>
-                    <td className="align-middle">
-                        <FormattedDate value={new Date(trabajo.fecha)}/>
-                    </td>
+        <Container>
+            <div>
+                <h4 className={"centeredParagraph"}>
+                    <FormattedMessage id={"trabajo.nombreLista"}/>
+                </h4>
+            </div>
+            <br/>
+            <table className="table table-striped table-hover">
+                <thead>
+                <tr>
+                    <th scope="col">
+                        <FormattedMessage id='trabajo.matricula' />
+                    </th>
+                    <th scope="col">
+                        <FormattedMessage id='trabajo.cliente' />
+                    </th>
+                    <th scope="col">
+                        <FormattedMessage id='trabajo.estado' />
+                    </th>
+                    <th scope="col">
+                        <FormattedMessage id='trabajo.fechaCreacion' />
+                    </th>
                 </tr>
-            )}
-            </tbody>
+                </thead>
 
-        </table>
+                <tbody>
+                {listaTrabajos.map(trabajo =>
+                    <tr key={trabajo.matricula}>
+                        <td>
+                            <Link to={`trabajos/${trabajo.idTrabajo}`}>{trabajo.matricula}</Link>
+                        </td>
+                        <td>
+                            {trabajo.nombreCliente}
+                        </td>
+                        <td className="align-middle">
+                            <Badge variant={commonFunctions.tipoLabelTrabajo(trabajo.estado)}>
+                                {intl.formatMessage({ id: 'trabajo.estado.' + trabajo.estado })}
+                            </Badge>
+                        </td>
+                        <td className="align-middle">
+                            <FormattedDate value={new Date(trabajo.fecha)}/>
+                        </td>
+                    </tr>
+                )}
+                </tbody>
+
+            </table>
+        </Container>
     )
 }
 
